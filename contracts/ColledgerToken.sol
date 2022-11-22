0da1921dc56d45ebcb07badbaaf5aaef0d80d3fa
Aman Yadav <goku.amyadav@gmail.com>
2022-11-22 22:42:27 +0530

    Initial commit II

---
+++ contracts/ColledgerToken.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Colledger is ERC20{
+
+    constructor() ERC20("Colledger","COL"){
+        
+    }
+}
