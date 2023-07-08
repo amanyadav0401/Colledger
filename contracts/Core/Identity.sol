7aef16e4dd788b9c9b2f4aa90754c2fadfc3f748
Aman Yadav <goku.amyadav@gmail.com>
2023-07-09 02:30:51 +0530

    Colledger Updated

---
+++ contracts/Core/Identity.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "@onchain-id/solidity/contracts/Identity.sol";
+
+interface IColledgerFactory {
+
+    function verifyIdentity(address _identity) external view returns(bool);
+
+}
+
+
+contract ColledgerIdentity {
+
+IColledgerFactory colledgerFactory;    
+
+uint identificationNumber;
+uint countryCode;
+string candidateName;
+
+address[] instituteCredentialAddresses;
+address[] employerCredentialAddresses;
+
+address currentEmployer;
+address currentInstitution;
+
+function verifyIdentity() external view returns(bool) {
+    return colledgerFactory.verifyIdentity(address(this));
+}
+
+}
